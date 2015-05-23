797ee10ee5f86b1d39f3617e38f28194dc7fca2b
Domen Blenkuš <domen@blenkus.com>
2015-05-23 20:11:19 +0200

    Updated vagrant provisioning script.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,7 +1,18 @@
-echo "Disabeling SELinux..."
-setenforce 0
-
-echo "Migrating database..."
 cd /webapps/rolcabox/rolca
 source ../bin/activate
-python manage.py migrate >/dev/null 2>&1
+
+echo "Updating requirements..."
+yum install -y libffi-devel >/dev/null
+pip install requests[security] >/dev/null
+pip install -Ur requirements.txt >/dev/null
+pip install ipython >/dev/null
+
+echo "Migrating database..."
+python manage.py migrate --noinput >/dev/null
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput >/dev/null
+chown -R rolcabox:webapps /webapps/rolcabox/static
+
+echo "Creating superuser..."
+DJANGO_USER_EMAIL='domen@blenkus.com' DJANGO_USER_PASSWORD='nm4xi6nb' python manage.py add_user --admin >/dev/null

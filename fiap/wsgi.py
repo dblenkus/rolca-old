8e758e659abf34baa3f4fc8e81d87f63feaee164
Domen Blenkuš <domen@blenkus.com>
2015-06-12 19:25:20 +0200

    Set rolcabox settings as default.

--- fiap/wsgi.py
+++ fiap/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiap.settings_dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiap.settings_rolcabox")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

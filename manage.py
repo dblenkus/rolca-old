99ff5e1faad52ac57ee1eb2a744969f323bdd446
Domen Blenkuš <domen@blenkus.com>
2015-06-13 00:41:41 +0200

    Run server on 0.0.0.0:8000 by default.

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiap.settings_rolcabox")
 
+    if len(sys.argv) == 2 and sys.argv[1] == 'runserver':
+        sys.argv.append('0.0.0.0:8000')
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)

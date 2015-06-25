67c1afcc596d48705d06e02a387e64ffec206604
Domen Blenkuš <domen@blenkus.com>
2015-06-25 12:25:31 +0200

    Updated requirements.

--- fiap/settings_rolcabox.py
+++ fiap/settings_rolcabox.py
@@ -21,10 +21,10 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        "BACKEND": "redis_cache.cache.RedisCache",
-        "LOCATION": "127.0.0.1:6379:0",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
         "OPTIONS": {
-            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }

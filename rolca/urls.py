a01eee95bba931e5f79a8c878893bf582698cd48
Domen Blenkuš <domen@blenkus.com>
2017-08-12 18:05:27 +0200

    Prepare 2017 settings

--- rolca/urls.py
+++ rolca/urls.py
@@ -31,8 +31,8 @@ urlpatterns = patterns(
     url(r'^natecaji/', include('uploader.urls')),
     url(r'^zirija/', include('jury.urls')),
 
-    # url(r'^$', RedirectView.as_view(url='/natecaji/'), name="index")
-    url(r'^$', RedirectView.as_view(url='/rezultati/'), name="index")
+    url(r'^$', RedirectView.as_view(url='/natecaji/'), name="index")
+    # url(r'^$', RedirectView.as_view(url='/rezultati/'), name="index")
     # url(r'^', include('frontend.urls')),
 )
 
